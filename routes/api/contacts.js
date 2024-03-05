96508154f1c257e12f71712840f5cde1b40c86e0
Wanderer1333 <wrestling1333@gmail.com>
2024-03-05 09:07:00 +0100

    wanderer1333

---
+++ routes/api/contacts.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const ctrl = require("../../controllers/contacts");
+
+router.get("/", ctrl.listContacts);
+
+router.get("/:contactId", ctrl.getContactById);
+
+router.post("/", ctrl.addContact);
+
+router.delete("/:contactId", ctrl.removeContact);
+
+router.put("/:contactId", ctrl.updateContact);
+
+module.exports = router;
