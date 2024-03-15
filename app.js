e2a22714fb3664fc0c2aee2219222ec5489aba79
Wanderer1333 <wrestling1333@gmail.com>
2024-03-16 00:10:31 +0100

    wanderer1333

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contacts");
+require("dotenv").config();
+
+const authRouter = require("./routes/api/userRouter");
+const contactsRouter = require("./routes/api/contactsRouter");
 
 const app = express();
 
@@ -12,6 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
